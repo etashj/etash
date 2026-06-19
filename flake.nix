d1e0f4b06264a7e8bfcec5da5ce46d4e9425dfac
Etash Jhanji <etashjhanji@gmail.com>
2026-06-19 11:38:08 -0400

    package management

--- flake.nix
+++ flake.nix
@@ -1,18 +1,25 @@
 {
-    description = "Development environment for Etash: Even That's A SHell";
+  description = "Development environment for Etash: Even That's A SHell";
 
-    inputs = {
-        nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
+  };
 
-    outputs = { self, nixpkgs }:
+  outputs =
+    { nixpkgs, ... }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      devShells = forAllSystems (system:
+      devShells = forAllSystems (
+        system:
         let
           pkgs = import nixpkgs { inherit system; };
         in
@@ -21,13 +28,23 @@
             packages = with pkgs; [
               git
 
+              nil
+              nixd
+
               cargo
+              rustc
+              rustfmt
+              shellcheck
+              shfmt
             ];
+
+            RUST_SRC_PATH = pkgs.rustPlatform.rustLibSrc;
 
             shellHook = ''
               echo "Etash development environment activated"
             '';
           };
-        });
+        }
+      );
     };
 }
